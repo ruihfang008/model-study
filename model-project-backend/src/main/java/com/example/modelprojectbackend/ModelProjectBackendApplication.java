87d321d1e3657c815aa5e1dc68dd658a4c713d38
{{ruihfang008}} <{{ruihfang@163.com}}>
2023-05-13 11:31:20 +0800

    创建项目

---
+++ model-project-backend/src/main/java/com/example/modelprojectbackend/ModelProjectBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.modelprojectbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ModelProjectBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ModelProjectBackendApplication.class, args);
+    }
+
+}
